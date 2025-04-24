05b9ef01ebd2cb2c35e18994aaa90680b4483d8e
pat <patricklouis.rivera.cics@ust.edu.ph>
2025-04-24 11:16:31 +0800

    Added with-nestjs folder, updated package.json

---
+++ examples/with-nestjs/scripts/develop.ts
@@ -0,0 +1,47 @@
+//node
+import { createServer } from 'node:http';
+//reactus
+import { dev } from 'reactus';
+
+async function develop() {
+  const cwd = process.cwd();
+  const engine = dev({
+    cwd,
+    basePath: '/',
+    watchIgnore: [ '**/.build/**' ],
+    //client script route prefix used in the document markup
+    //ie. /client/[id][extname]
+    //<script type="module" src="/client/[id][extname]"></script>
+    //<script type="module" src="/client/abc123.tsx"></script>
+    clientRoute: '/client'
+  });
+
+  const server = createServer(async (req, res) => {
+    //handles public, assets and hmr
+    await engine.http(req, res);
+    //if middleware was triggered
+    if (res.headersSent) return;
+    // home page
+    if (req.url === '/') {
+      res.setHeader('Content-Type', 'text/html');
+      res.end(await engine.render('@/pages/home', { title: 'Home' }));
+      return;
+    //about page
+    } else if (req.url === '/about') {
+      res.setHeader('Content-Type', 'text/html');
+      res.end(await engine.render('@/pages/about'));
+      return;
+    }
+    res.end('404 Not Found');
+  });
+
+  server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000/');
+  });
+}
+
+develop().catch(e => {
+  console.error(e);
+  process.exit(1);
+});
+
---
+++ examples/with-nestjs/scripts/start.ts
@@ -0,0 +1,58 @@
+//node
+import { createServer } from 'node:http';
+import path from 'node:path';
+import sirv from 'sirv';
+//reactus
+import { serve } from 'reactus';
+
+async function start() {
+  const cwd = process.cwd();
+  const engine = serve({
+    cwd,
+    //ie. /client/[id][extname]
+    //<script type="module" src="/client/[id][extname]"></script>
+    //<script type="module" src="/client/abc123.tsx"></script>
+    clientRoute: '/client',
+    //path where to load the server script (js)
+    pagePath: path.join(cwd, '.build/pages'),
+    //css route prefix used in the document markup
+    //ie. /assets/[id][extname]
+    //<link rel="stylesheet" type="text/css" href="/client/[id][extname]" />
+    //<link rel="stylesheet" type="text/css" href="/assets/abc123.css" />
+    cssRoute: '/assets'
+  });
+  // Init `sirv` handler
+  const assets = sirv(path.join(cwd, 'public'), {
+    maxAge: 31536000, // 1Y
+    immutable: true
+  });
+
+  const server = createServer(async (req, res) => {
+    // home page
+    if (req.url === '/') {
+      res.setHeader('Content-Type', 'text/html');
+      res.end(await engine.render('@/pages/home'));
+      return;
+    //about page
+    } else if (req.url === '/about') {
+      res.setHeader('Content-Type', 'text/html');
+      res.end(await engine.render('@/pages/about'));
+      return;
+    }
+    //static asset server
+    assets(req, res);
+    //if static asset was triggered
+    if (res.headersSent) return;
+    res.end('404 Not Found');
+  });
+
+  server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000/');
+  });
+}
+
+start().catch(e => {
+  console.error(e);
+  process.exit(1);
+});
+
